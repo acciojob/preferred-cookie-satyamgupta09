856ea724fb25c1ff3cdfd81d9016f27945eac0c2
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-03-02 01:19:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function getCookie(name) {
   let cookieArr = document.cookie.split(';');
   for (let i = 0; i < cookieArr.length; i++) {
     let cookie = cookieArr[i].trim();
-    if (cookie.indexOf(name + "=") === 0) {
+    if (cookie.indexOf(name  "=") === 0) {
       return cookie.substring(name.length + 1, cookie.length);
     }
   }
